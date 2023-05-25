f8388cb549ae0d2d620a279652d9c0495ec11e73
Ebin Joshy Nambiaparambil <ebinjoshy@Ebins-MacBook-Pro.local>
2023-05-25 19:43:12 +1000

    fix: Updated console logging in  server

--- services/server/src/index.js
+++ services/server/src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // CREATE Endpoint
 app.post("/issues", function (req, res) {
@@ -8,39 +12,45 @@ app.post("/issues", function (req, res) {
   const data = req.body;
 
   // Print json
+  console.log("Created Issue");
   console.log(data);
 
   // Return a success
-  res.send(201);
+  res.send("Issue created");
 });
 
 // GET Endpoint
 app.get("/issues/:id", function (req, res) {
   // Return JSON response.
-  res.json({ id: "1", first_name: "Hey", surname: "Jude" });
-
-  // Return a success
-  res.send(200);
+  res.json({
+    id: "1",
+    title: "Test Issue",
+    description: "Test Description",
+    status: "in-development",
+  });
 });
 
 // UPDATE Endpoint
-app.patch("/issues/:id", function (req, res) {
+app.patch("/issues/:id", function (request, res) {
+  const data = request.body;
+  console.log("Updated Issue");
   // Print json
   console.log(data);
 
   // Return a success
-  res.send(200);
+  res.send("Issue updated");
 });
 
 // DELETE Endpoint
 app.delete("/issues/:id", function (req, res) {
   // Get the resource from the database.
 
+  console.log("Deleted Issue");
   // Id to delete
   console.log(req.params.id);
 
-  // Return a success
-  res.send(200);
+  // Return a successç
+  res.send("Issue deleted");
 });
 
 app.listen(3000, function () {
